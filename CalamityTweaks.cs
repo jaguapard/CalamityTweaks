ae9c1d2d71a225eb30958991db8a2e07ae3697d3
groomyjohny <groomyjohny@gmail.com>
2022-09-23 18:12:18 +0500

    Implemented very barebones Supreme Cnidrion

--- CalamityTweaks.cs
+++ CalamityTweaks.cs
@@ -6,3 +6,29 @@ namespace CalamityTweaks
 	{
 	}
 }
+
+namespace CalamityTweaks.Enemies
+{
+	public class SupremeCnidrion : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Supreme Cnidrion");
+			
+		}
+
+		public override void SetDefaults()
+		{
+			NPC.width = 300;
+			NPC.height = 120;
+			NPC.damage = 350;
+			NPC.defense = 150;
+			NPC.lifeMax = 3000000;
+			NPC.knockBackResist = 0;
+			NPC.boss = true;
+			NPC.noGravity = true;
+			NPC.value = 100 * 100 * 100 * 100; //100 platinum. TODO: remove magic number
+			NPC.aiStyle = -1;
+		}
+	}
+}

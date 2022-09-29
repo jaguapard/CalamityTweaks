81c419635cf93184d0a79b5518de5239f06903f8
groomyjohny <groomyjohny@gmail.com>
2022-09-29 11:13:17 +0500

    Separated helpers into their own file

---
+++ src/Helpers/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using System;
+
+namespace CalamityTweaks.Helpers
+{
+    public static class Funcs
+    {
+        public static bool InRange(int val, int min, int max)
+        {
+            return val >= min && val <= max;
+        }
+
+        public static Vector2 toPolar(Vector2 pos)
+        {
+            return new(pos.Length(), MathF.Atan2(pos.Y, pos.X));
+        }
+    }
+}
